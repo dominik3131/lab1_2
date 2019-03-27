65cebe2b6f362d6540754fc0c5a884e2ebc8905c
Dominik <rakdominikrak@gmail.com>
2019-03-27 18:11:48 +0100

    shouldThrowIllegalArgumentExceptionWithCurrencyMismatchWhenAdding()

--- src/test/java/lab1_2/MoneyTests.java
+++ src/test/java/lab1_2/MoneyTests.java
@@ -3,6 +3,8 @@ package lab1_2;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import java.util.Currency;
+
 import org.junit.Test;
 
 import pl.com.bottega.ecommerce.sharedkernel.Money;
@@ -15,5 +17,12 @@ public class MoneyTests {
         Money moneyAfterMultiplying = new Money(200);
         money = money.multiplyBy(2);
         assertThat(moneyAfterMultiplying.equals(money), equalTo(true));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionWithCurrencyMismatchWhenAdding() {
+        Money eurMoney = new Money(1, Currency.getInstance("EUR"));
+        Money usdMoney = new Money(1, Currency.getInstance("USD"));
+        eurMoney.add(usdMoney);
     }
 }
